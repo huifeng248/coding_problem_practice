68cf30ef4cf99ef37b2456b7c04c2d50d58b6c94
Hui Feng <huifeng@MacBook-Pro-3.attlocal.net>
2023-01-05 18:52:12 -0800

    max path sum

--- dynamic_programming/max_path_sum.py
+++ dynamic_programming/max_path_sum.py
@@ -81,3 +81,30 @@ grid = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 max_path_sum(grid) # -> 56
+
+
+# solution
+# dynamic programming with memoization
+def max_path_sum(grid):
+  return _max_path_sum(grid, 0, 0, {})
+
+def _max_path_sum(grid, r, c, memo):
+  pos = (r, c)
+  if pos in memo:
+    return memo[pos]
+  
+  if r == len(grid) or c == len(grid[0]):
+    return float('-inf')
+  
+  if r == len(grid) - 1 and c == len(grid[0]) - 1:
+    return grid[r][c]
+  
+  down = _max_path_sum(grid, r + 1, c, memo)
+  right = _max_path_sum(grid, r, c + 1, memo)  
+  
+  memo[pos] = grid[r][c] + max(down, right)
+  return memo[pos]
+# r = # rows
+# c = # columns
+# Time: O(r*c)
+# Space: O(r*c)
