133bdb00e8ba8a44755a3fc186f5adc80219720a
Hui Feng <huifeng@MacBook-Pro-3.attlocal.net>
2023-01-31 12:19:12 -0800

    mix

---
+++ mix/positioning_plants.py
@@ -0,0 +1,145 @@
+def positioning_plants(costs):
+  res = _positioning_plants(costs, 0, None, {})
+  return res
+  
+def _positioning_plants(costs, current_position, previous_position, memo):
+  key = (current_position, previous_position)
+  if key in memo:
+    return memo[key]
+  
+  if current_position == len(costs):
+    return 0
+
+  lowest_cost = float('inf')
+  for position_index, product_cost in enumerate(costs[current_position]):
+    if position_index != previous_position:
+      option = product_cost + _positioning_plants(costs, current_position+1, position_index, memo)
+      lowest_cost = min(lowest_cost, option)
+  
+  memo[key] = lowest_cost
+  return lowest_cost
+  
+    
+print(positioning_plants([
+  [4, 3, 7],
+  [6, 1, 9],
+  [2, 5, 3]
+])) # -> 7, by doing 4 + 1 + 2.)
+
+
+# solution
+# dynamic programming with memoization
+def positioning_plants(costs):
+  return _positioning_plants(costs, 0, None, {})
+
+def _positioning_plants(costs, pos, last_plant, memo):
+  key = (pos, last_plant)
+  if key in memo:
+    return memo[key]
+  
+  if pos == len(costs):
+    return 0
+  
+  min_cost = float('inf')
+  
+  for plant, cost in enumerate(costs[pos]):
+    if plant != last_plant:
+      candidate = cost + _positioning_plants(costs, pos + 1, plant, memo)
+      min_cost = min(candidate, min_cost)
+  
+  memo[key] = min_cost
+  return min_cost
+# n = # of garden positions (rows)
+# m = # of plant types (columns)
+# Time: O(nm)
+# Space: O(nm)
+
+
+# positioning plants
+# You've been hired to plant flowers in a garden with n different positions. There are m different flower types. The prices of flowers types vary depending on which position they are planted. Your bosses are picky, they tell you to never plant two of the same flower type right next to each other. What is the minimum cost we need to plant a flower in each position of the garden?
+
+# Write a function, positioningPlants, that takes in a 2D list with dimensions n * m. Each row of the list represents the costs of the flower types at that position. This means that costs[i][j] represents the cost of planting flower type j at position i. For example:
+
+# Given these costs,
+
+costs = [
+  [4, 3, 7],
+  [6, 1, 9],
+  [2, 5, 3]
+]
+
+# The costs of plants at position 1 are $6, $1, and $9.
+# The cost of planting flower type 0 at position 1 is $6.
+# The cost of planting flower type 2 at position 1 is $9.
+# The function should return the minimum cost of planting flowers without placing the same flower type in adjacent positions.
+
+# test_00:
+positioning_plants([
+  [4, 3, 7],
+  [6, 1, 9],
+  [2, 5, 3]
+]) # -> 7, by doing 4 + 1 + 2.
+# test_01:
+positioning_plants([
+  [12, 14, 5],
+  [6, 3, 2]
+]) # -> 8
+# test_02:
+positioning_plants([
+  [12, 14, 5],
+  [6, 3, 2],
+  [4, 2, 7],
+  [4, 8, 4],
+  [1, 13, 5],
+  [8, 6, 7],
+]) # -> 23
+# test_03:
+positioning_plants([
+  [12, 14, 5, 13],
+  [6, 3, 20, 3],
+  [24, 12, 7, 2],
+  [4, 80, 45, 3],
+  [104, 13, 5, 14],
+  [38, 19, 7, 6],
+  [12, 2, 1, 2],
+]) # -> 26
+# test_04:
+positioning_plants([
+  [12, 14, 50, 12],
+  [6, 3, 20, 3],
+  [24, 12, 7, 2],
+  [4, 80, 45, 3],
+  [104, 13, 5, 14],
+  [38, 19, 7, 6],
+  [1, 20, 1, 2],
+  [13, 12, 5, 13],
+  [60, 32, 20, 3],
+  [24, 12, 7, 2],
+  [4, 80, 44, 1],
+  [104, 13, 5, 14],
+  [38, 19, 76, 6],
+  [12, 23, 12, 20],
+  [1, 3, 1, 1],
+  [1, 2, 12, 5],
+]) # -> 74
+# test_05:
+positioning_plants([
+  [12, 14, 50, 12, 13],
+  [6, 3, 20, 3, 16],
+  [24, 12, 7, 2, 74],
+  [4, 80, 45, 3, 100],
+  [104, 13, 5, 14, 3],
+  [38, 19, 7, 6, 24],
+  [1, 20, 1, 2, 31],
+  [13, 12, 5, 13, 9],
+  [60, 32, 20, 3, 2],
+  [24, 12, 7, 2, 42],
+  [4, 80, 44, 1, 23],
+  [104, 13, 5, 14, 28],
+  [38, 19, 76, 6, 12],
+  [12, 23, 12, 20, 13],
+  [1, 3, 1, 1, 50],
+  [1, 2, 12, 5, 36],
+  [6, 2, 3, 12, 20],
+  [4, 6, 4, 11, 15],
+]) # -> 75
