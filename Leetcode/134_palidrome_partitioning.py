3f1ef762bf930e3e862aec69c5d82266f5081e12
Hui Feng <huifeng@MacBook-Pro-3.attlocal.net>
2023-03-11 01:24:36 -0800

    134

--- Leetcode/134_palidrome_partitioning.py
+++ Leetcode/134_palidrome_partitioning.py
@@ -18,18 +18,20 @@ class Solution:
                     print("come here", path, "popped", popped)
 
                 print("outside~~~~~~~~", s[i:j+1], "res", res)
-            return res
+            
 
         def is_pali(s, i, j):
             sub_str = s[i:j+1]
             return sub_str == sub_str[::-1]
         
-        return backtracking(0)
+        backtracking(0)
+        return res
     
-a = "aab"
+a = "ab"
 s = Solution()
 print(s.partition(a))
 
 # The time complexity of this code is O(n * 2^n), where n is the length of the input string s. This is because the backtracking function generates all possible partitions of the input string, which is of size 2^n. For each partition, the is_pali function checks if it is a palindrome, which takes O(n) time. Therefore, the total time complexity is O(n * 2^n).
 
-# The space complexity of this code is also O(n * 2^n), since the backtracking function generates all possible partitions, each of which has a length of up to n. The res and path lists also contribute to the space complexity, but since they are cleared after each recursive call, their total space usage is proportional to the number of partitions generated, which is 2^n. Therefore, the space complexity is O(n * 2^n).
+
+# Space Complexity: O(N)\mathcal{O}(N)O(N), where NNN is the length of the string sss. This space will be used to store the recursion stack. For s = aaa, the maximum depth of the recursive call stack is 3 which is equivalent to NNN.
