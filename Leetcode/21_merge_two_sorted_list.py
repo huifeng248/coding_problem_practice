11625e40f6bbf175818628358b076e39c948d8d7
Hui Feng <huifeng@MacBook-Pro-3.attlocal.net>
2023-03-14 00:29:30 -0700

    21

--- Leetcode/21_merge_two_sorted_list.py
+++ Leetcode/21_merge_two_sorted_list.py
@@ -33,3 +33,23 @@ class Solution:
         #     current.next = current1
         current.next = current1 if current1 is not None else current2
         return dummy_head.next
+    
+# Time complexity : O(n+m), Because exactly one of l1 and l2 is incremented on each loop iteration, the while loop runs for a number of iterations equal to the sum of the lengths of the two lists. All other work is constant, so the overall complexity is linear.
+
+# Space complexity : O(1), The iterative approach only allocates a few pointers, so it has a constant overall memory footprint.
+    #recursion 
+    
+    class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+# Time complexity : O(n+m),
+# Space complexity : O(n+m),
