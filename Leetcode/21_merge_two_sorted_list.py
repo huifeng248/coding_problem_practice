28c16c8d10bde948efafca7f1e9404e43c1fe6fb
Hui Feng <huifeng@MacBook-Pro-3.attlocal.net>
2023-04-19 10:43:03 -0700

    21

--- Leetcode/21_merge_two_sorted_list.py
+++ Leetcode/21_merge_two_sorted_list.py
@@ -3,9 +3,42 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+# # interation 
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         if not list1 and not list2:
+#             return None
+#         if not list1:
+#             return list2
+#         if not list2:
+#             return list1
+
+        # dummy_head = ListNode(None)
+        # current = dummy_head
+        # while list1 and list2:
+        #     if list1.val <= list2.val:
+        #         current.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         current.next = list2
+        #         list2 = list2.next
+            
+        #     current = current.next
+        
+        # if not list1:
+        #     current.next = list2
+        # if not list2:
+        #     current.next = list1
+        # return dummy_head.next
+
+
+# time complexity: O(m+n)
+# space complexity: O(1)
+
+# recursion 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        print("come here")
         if not list1 and not list2:
             return None
         if not list1:
@@ -13,43 +46,18 @@ class Solution:
         if not list2:
             return list1
         
-        dummy_head = ListNode(0)
-        current1 = list1
-        current2 = list2
-        current = dummy_head
-
-        while current1 and current2:
-            if current1.val <= current2.val:
-                current.next = current1
-                current1 = current1.next
-            else:
-                current.next = current2
-                current2 = current2.next
-            current = current.next
-
-        # if not current1:
-        #     current.next = current2
-        # if not current2:
-        #     current.next = current1
-        current.next = current1 if current1 is not None else current2
-        return dummy_head.next
-    
-# Time complexity : O(n+m), Because exactly one of l1 and l2 is incremented on each loop iteration, the while loop runs for a number of iterations equal to the sum of the lengths of the two lists. All other work is constant, so the overall complexity is linear.
-
-# Space complexity : O(1), The iterative approach only allocates a few pointers, so it has a constant overall memory footprint.
-    #recursion 
-    
-    class Solution:
-    def mergeTwoLists(self, l1, l2):
-        if l1 is None:
-            return l2
-        elif l2 is None:
-            return l1
-        elif l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
         else:
-            l2.next = self.mergeTwoLists(l1, l2.next)
-            return l2
-# Time complexity : O(n+m),
-# Space complexity : O(n+m),
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2
+
+# time complexity: O(m+n)
+# space complexity: O(m+n)
+
+
+
+
+
+        
