8398873fad19fc55ae3823dc88f690b9bb662ae3
Hui Feng <huifeng@MacBook-Pro-3.attlocal.net>
2023-05-06 15:15:45 -0700

    113

--- Leetcode/113_path_sum.py
+++ Leetcode/113_path_sum.py
@@ -4,6 +4,34 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# recursive 
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        self.find_path(root, [root.val], root.val, targetSum, res)
+        return res
+    def find_path(self, current, path, accu, targetSum, res):
+        if not current:
+            return 
+        
+      
+        if not current.right and not current.left and accu == targetSum:
+            res.append(path)
+            return 
+        
+        if current.left:
+            self.find_path(current.left, path+[current.left.val], accu+current.left.val, targetSum, res)
+        if current.right:
+            self.find_path(current.right, path+[current.right.val], accu+current.right.val, targetSum, res)
+
+        
+
+
+
+
 #iteration
 # class Solution:
 #     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
@@ -26,24 +54,24 @@
         
 #         return res
 
-class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        if not root:
-            return []
-        res = []
-        stack = [[root, [root.val]]]
-        while stack:
-            current, path = stack.pop()
-            total = sum(path)
-            if not current.left and not current.right and total == targetSum:
-                res.append(path)
+# class Solution:
+#     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+#         if not root:
+#             return []
+#         res = []
+#         stack = [[root, [root.val]]]
+#         while stack:
+#             current, path = stack.pop()
+#             total = sum(path)
+#             if not current.left and not current.right and total == targetSum:
+#                 res.append(path)
             
-            if current.left:
-                stack.append([current.left, path+[current.left.val]])
-            if current.right:
-                stack.append([current.right, path+[current.right.val]])
+#             if current.left:
+#                 stack.append([current.left, path+[current.left.val]])
+#             if current.right:
+#                 stack.append([current.right, path+[current.right.val]])
         
-        return res
+#         return res
 
 
 # more optimized iteration solution 
